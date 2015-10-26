f4917c5c952675cb16fcde6971d36ccc26df1ca1
Jared Morgan <jmorgan@mchost.net>
2015-10-26 13:46:20 -0500

    Basic input setup

---
+++ AuTouch/NetworkOperation.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkOperation.swift
+//  AuTouch
+//
+//  Created by Jared Morgan on 10/18/15.
+//  Copyright © 2015 Jared Morgan. All rights reserved.
+//
+
+import Foundation
+
+class NetworkOperation {
+    lazy var config: NSURLSessionConfiguration =
+        NSURLSessionConfiguration.defaultSessionConfiguration()
+    lazy var session: NSURLSession =
+        NSURLSession(configuration: self.config)
+    let queryURL: NSURL
+    
+    typealias JSONDictionaryCompletion = [String: AnyObject]? -> Void
+    
+    init(url: NSURL) {
+        self.queryURL = url
+    }
+    
+    func post(params: Dictionary<String, AnyObject>) -> Void {
+        let request: NSMutableURLRequest = NSMutableURLRequest(URL: queryURL)
+        do {
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(
+                params, options: NSJSONWritingOptions.PrettyPrinted
+            )
+            request.HTTPMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+        } catch {
+            print("Error:\n \(error)")
+            return
+        }
+        
+        let dataTask = session.dataTaskWithRequest(request) {
+            (let data, let response, let error) in
+        }
+        
+        dataTask.resume()
+    }
+}
--- AuTouch/ViewController.swift
+++ AuTouch/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var box0: UILabel!
+    @IBOutlet weak var box1: UILabel!
+    @IBOutlet weak var box2: UILabel!
+    @IBOutlet weak var box3: UILabel!
+    @IBOutlet weak var box4: UILabel!
+    @IBOutlet weak var box5: UILabel!
+    @IBOutlet weak var box6: UILabel!
+    @IBOutlet weak var box7: UILabel!
+    @IBOutlet weak var box8: UILabel!
+    @IBOutlet weak var box9: UILabel!
+    @IBOutlet weak var box10: UILabel!
+    @IBOutlet weak var box11: UILabel!
+    
+    var boxes: [UILabel] = []
+    
+    let touchURL = NSURL(string: "http://autouch.dev:6001/touches")
+    let formatter = NSDateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        boxes += [
+            box0, box1, box2, box3, box4, box5,
+            box6, box7, box8, box9, box10, box11
+        ]
+        
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        formatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+        formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +46,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addTap(sender: AnyObject) {
+        let networkOperation = NetworkOperation(url: touchURL!)
+        
+        let boxView = sender.view as! UILabel
+        if let index = boxes.indexOf(boxView) {
+            let params = [
+                "box": index,
+                "touch_at": formatter.stringFromDate(NSDate())
+            ]
+            networkOperation.post(["touch": params])
+        }
+    }
 }
-
